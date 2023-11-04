a2a2bf294aa9d58cd1ff39efb2497439261b9b60
Andreas Duerloo <andrreasduerloo@gmail.com>
2023-11-04 11:59:13 +0100

    Day 11 done

--- day_11_monkeys/day_11.go
+++ day_11_monkeys/day_11.go
@@ -7,13 +7,34 @@ import (
 )
 
 func Solve() (int, int) {
-	input, err := os.ReadFile("inputs/12.txt")
+	input, err := os.ReadFile("inputs/11.txt")
 	if err != nil {
 		fmt.Println("Could not read the input file - exiting")
 		return 0, 0
 	}
 
 	monkeys := parse(strings.Split(string(input), "\n\n"))
+	magicNum := magicNumber(monkeys)
 
-	return 0, 0
+	// First star - twenty rounds
+	for i := 0; i < 20; i++ {
+		for _, m := range monkeys {
+			inspectAndThrowItems(m, false, magicNum)
+		}
+	}
+
+	first := monkeyBusiness(monkeys)
+
+	// Second star - reset the monkeys
+	monkeys = parse(strings.Split(string(input), "\n\n"))
+
+	for i := 0; i < 10000; i++ {
+		for _, m := range monkeys {
+			inspectAndThrowItems(m, true, magicNum)
+		}
+	}
+
+	second := monkeyBusiness(monkeys)
+
+	return first, second
 }
--- day_11_monkeys/lib.go
+++ day_11_monkeys/lib.go
@@ -2,18 +2,19 @@ package day_11
 
 import (
 	"advent2022/helpers"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 type monkey struct {
-	items   []int
-	action  rule
-	div     int
-	targets []int
-	ifTrue  *monkey
-	ifFalse *monkey
-	thrown  int
+	items     []int
+	action    rule
+	div       int
+	targets   []int
+	ifTrue    *monkey
+	ifFalse   *monkey
+	inspected int
 }
 
 type rule struct {
@@ -56,14 +57,71 @@ func newMonkey(desc string) *monkey {
 		div:     helpers.ReGetInts(lines[3])[0],
 		targets: []int{trueInt, falseInt},
 	}
-
 	return &out
 }
 
-func inspectAndThrow() {
-	//
+// This function takes a pointer to a monkey and a boolean to indicate what star we're solving.
+// True is the second star
+func inspectAndThrowItems(m *monkey, s bool, magicNum int) {
+	for _, item := range m.items {
+		// Inspect the item: apply the rule
+		worryVal := applyRule(m, item)
+		if worryVal > magicNum { // Math magic
+			worryVal = worryVal % magicNum
+		}
+		m.inspected += 1
+
+		// Brief relief
+		if !s {
+			worryVal = worryVal / 3
+		}
+
+		// Test and throw
+		if worryVal%m.div == 0 {
+			m.ifTrue.items = append(m.ifTrue.items, worryVal)
+		} else {
+			m.ifFalse.items = append(m.ifFalse.items, worryVal)
+		}
+	}
+
+	// Empty this monkey's item queue
+	m.items = make([]int, 0)
 }
 
-func applyRule() {
-	//
+func applyRule(m *monkey, v int) int {
+	var otherVal int
+	switch m.action.value {
+	case "old":
+		otherVal = v
+	default:
+		otherVal, _ = strconv.Atoi(m.action.value)
+	}
+
+	switch m.action.operation {
+	case "+":
+		v = v + otherVal
+	case "*":
+		v = v * otherVal
+	}
+	return v
+}
+
+func monkeyBusiness(monkeys []*monkey) int {
+	// Order the slice of monkeys by 'inspected'
+	slices.SortFunc(monkeys, func(a, b *monkey) int { return b.inspected - a.inspected })
+
+	// Take the top two and multiply their values
+	return monkeys[0].inspected * monkeys[1].inspected
+}
+
+// Returns the product of all monkey.div values
+// Needed for moludar arithmetic alchemy
+func magicNumber(monkeys []*monkey) int {
+	out := 1
+
+	for _, m := range monkeys {
+		out *= m.div
+	}
+
+	return out
 }
